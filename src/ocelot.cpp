0a2e218dce856716184989ccaa752d7ad27c0093
Daniel Kamil Kozar <dkk089@gmail.com>
2018-01-20 04:13:35 +0100

    Fix compilation errors
    Missing semicolon, extraneous parenthesis, and an API change in spdlog.

--- src/ocelot.cpp
+++ src/ocelot.cpp
@@ -125,7 +125,7 @@ int main(int argc, char **argv) {
 		conf->load(conf_file_path, conf_file);
 	}
 
-	if (conf->get_bool("daemonize") || daemonize)) {
+	if (conf->get_bool("daemonize") || daemonize) {
 		create_daemon();
 	}
 
@@ -134,7 +134,7 @@ int main(int argc, char **argv) {
 		sinks.push_back(std::make_shared<spdlog::sinks::stdout_sink_st>());
 	}
 	if (conf->get_bool("log") && !conf->get_str("log_path").empty()) {
-		sinks.push_back(std::make_shared<spdlog::sinks::daily_file_sink_st>(conf->get_str("log_path"), "log", 23, 59));
+		sinks.push_back(std::make_shared<spdlog::sinks::daily_file_sink_st>(conf->get_str("log_path"), 23, 59));
 	}
 
 	auto combined_logger = std::make_shared<spdlog::logger>("logger", begin(sinks), end(sinks));
